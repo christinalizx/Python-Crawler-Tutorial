f6d644f0a899686dc0c4325dcaf6fb9f00991179
christinalizx <120531026+christinalizx@users.noreply.github.com>
2023-03-09 15:48:59 -0500

    Update basic.py

--- basic.py
+++ basic.py
@@ -4,7 +4,7 @@ import requests
 # head = {"User-Agent": "Mozilla/5.0 (Windows NT1 0.0; Win64; x64)"}
 # This is a way to hide the actual identification of your equipment.
 # It is more commonly used to pretend ourselves as a normal browser rather than a crawler.
-response = requests.get("http://book.toescape.com/")  # add a parameter after this URL, header = head
+response = requests.get("http://book.toescape.com/")  # add a parameter after this URL, headers = head
 if 200 <= response.status_code < 400:  # if response.ok == true:
     print(response.text)
 elif 400 <= response.status_code < 500:
